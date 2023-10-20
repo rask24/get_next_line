0661d84e53450d489248d9909e2a2410fc25105a
rask24 <70057885+rask24@users.noreply.github.com>
2023-10-20 18:14:04 +0900

    modify ft_strjoin_with_free (#2)

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: reasuke <reasuke@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 16:03:32 by reasuke           #+#    #+#             */
-/*   Updated: 2023/10/16 15:10:50 by reasuke          ###   ########.fr       */
+/*   Updated: 2023/10/19 21:23:20 by reasuke          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,15 +77,19 @@ char	*ft_strjoin_with_free(const char *s1, const char *s2, int free_flag)
 	if (!s1 && !s2)
 		return (NULL);
 	ret = malloc(s1_len + s2_len + 1);
-	if (!ret)
-		return (NULL);
-	if (s1)
+	if (ret && s1)
 		ft_strlcpy(ret, s1, s1_len + 1);
-	if (s2)
+	if (ret && s2)
 		ft_strlcpy(ret + s1_len, s2, s2_len + 1);
 	if (s1 && free_flag & 0b10)
+	{
 		free((char *)s1);
+		s1 = NULL;
+	}
 	if (s2 && free_flag & 0b01)
+	{
 		free((char *)s2);
+		s2 = NULL;
+	}
 	return (ret);
 }
